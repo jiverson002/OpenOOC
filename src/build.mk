44c9d72e45e0b97b04434279d7dfac1179117064
jeremy <jliverson5@gmail.com>
2016-02-29 23:26:45 -0600

    added fiber.c to start implementing fiber management

--- src/build.mk
+++ src/build.mk
@@ -1,3 +1,3 @@
 src_LIBRARIES := libooc.a
 
-libooc.a_SOURCES := sigsegv.c
+libooc.a_SOURCES := fiber.c sigsegv.c
--- src/include/ooc.h
+++ src/include/ooc.h
@@ -8,8 +8,22 @@
 #define OPENOOC_RCAND -pre
 
 
+/* jmp_buf */
+#include <setjmp.h>
+
 /* ucontext_t */
 #include <ucontext.h>
+
+
+/* Maximum number of fibers per thread. */
+#define OOC_NUM_FIBERS 10
+
+
+/* An execution context, henceforth known as a fiber. */
+typedef struct ooc_fiber
+{
+  ucontext_t uc;
+} ooc_fiber_t;
 
 
 /* The stack environment to return to via longjmp. */
@@ -18,11 +32,14 @@ extern __thread jmp_buf ooc_ret_env;
 /* The thread context at the point SIGSEGV was raised. */
 extern __thread ucontext_t ooc_ret_uc;
 
+/* The fibers. */
+extern __thread ooc_fiber_t ooc_fiber[OOC_NUM_FIBERS];
+
 
 /* The OOC black magic. */
 #define OOC_FOR(loops) \
   for (loops) {\
-    int _ret/*, _run*/;\
+    int _ret, _me, _run;\
 \
     /* Set SIGSEGV return point. */\
     _ret = setjmp(ooc_ret_env);\
@@ -30,10 +47,10 @@ extern __thread ucontext_t ooc_ret_uc;
     /* If we arrived here via longjmp. */\
     if (_ret) {\
       /* Save my pre-sigsegv context. */\
-      /*  memcpy(&(fiber[me].uc), &ooc_ret_uc, sizeof(ooc_ret_uc));*/\
+      /*  memcpy(&(fiber[_me].uc), &ooc_ret_uc, sizeof(ooc_ret_uc));*/\
 \
       /* Find a fiber that is runnable */\
-      /*  _run = -1;*/\
+      _run = -1;\
       /*  ???*/\
 \
       /* If no fiber is runnable and there are more available fiber slots,\
@@ -46,7 +63,7 @@ extern __thread ucontext_t ooc_ret_uc;
       /*  }*/\
 \
       /* Switch to the runnable fiber */\
-      /*  setcontext(&(fiber[run].uc));*/\
+      setcontext(&(ooc_fiber[_run].uc));\
     }
 #define OOC_DO \
     {
