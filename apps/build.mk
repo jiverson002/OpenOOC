a2548d607250f9a6cf63c0ff748f6c67c90209ff
jeremy <jliverson5@gmail.com>
2016-03-01 22:56:41 -0600

    removed mm-hc and made mm the code to use OOC_FOR in.

--- apps/build.mk
+++ apps/build.mk
@@ -1,8 +1,5 @@
-apps_PROGRAMS := mm mm-hc
-apps_CFLAGS   := -Wno-unused-function
+apps_PROGRAMS := mm
 apps_LDADD    := libooc.a
 
 mm_SOURCES := mm.c
-
-mm-hc_SOURCES := mm-hc.c
-mm-hc_LDLIBS  := -lrt
+mm_LDLIBS  := -lrt
--- apps/mm.c
+++ apps/mm.c
@@ -1,7 +1,54 @@
+/*
+Copyright (c) 2016 Jeremy Iverson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+/* assert */
+#include <assert.h>
+
+/* setjmp */
+#include <setjmp.h>
+
+/* sigaction */
+#include <signal.h>
+
+/* EXIT_SUCCESS */
 #include <stdlib.h>
+
+/* memset */
+#include <string.h>
+
+/* mmap, munmap, PROT_NONE, MAP_PRIVATE, MAP_ANONYMOUS */
+#include <sys/mman.h>
+
+/* OOC library */
+#include "src/ooc.h"
 
 
 #define restrict
+
+
+/* Function prototype */
+void
+ooc_sigsegv(int const _sig, siginfo_t * const _si, void * const _uc);
 
 
 static void
@@ -31,15 +78,15 @@ mm(
 #define b(R,C) b[R*p+C]
 #define c(R,C) c[R*p+C]
 
-//#pragma omp parallel for schedule(static) default(none) private(j,k)
-  for (i=0; i<m; ++i) {
+  OOC_FOR(i=0; i<m; ++i)
+  OOC_DO
     for (j=0; j<n; ++j) {
       c(i,j) = a(i,0)*b(0,j);
       for (k=1; k<p; ++k) {
         c(i,j) += a(i,k)*b(k,j);
       }
     }
-  }
+  OOC_DONE
 
 #undef a
 #undef b
@@ -53,7 +100,35 @@ main(
   char * argv[]
 )
 {
-  if (0 == argc || (void*)0 == argv) {}
+  int ret;
+  size_t m, n, p;
+  struct sigaction act;
+  double * a, * b, * c;
+
+  memset(&act, 0, sizeof(act));
+  act.sa_sigaction = &ooc_sigsegv;
+  act.sa_flags = SA_SIGINFO;
+  ret = sigaction(SIGSEGV, &act, NULL);
+  assert(!ret);
+
+  m = 100;
+  n = 100;
+  p = 100;
+
+  a = mmap(NULL, m*n*sizeof(*a), PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  assert(a);
+  b = mmap(NULL, n*p*sizeof(*b), PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  assert(b);
+  c = mmap(NULL, m*p*sizeof(*c), PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  assert(c);
+
+  mm(m, n, p, a, b, c);
+
+  munmap(a, m*n*sizeof(*a));
+  munmap(b, n*p*sizeof(*b));
+  munmap(c, m*p*sizeof(*c));
 
   return EXIT_SUCCESS;
+
+  if (argc || argv) {}
 }
