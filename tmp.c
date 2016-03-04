d013e9701144cf69fabf61b37ea43140fb3c45a3
jeremy <jliverson5@gmail.com>
2016-03-04 01:06:02 -0600

    more messing with preprocessor

--- tmp.c
+++ tmp.c
@@ -1,3 +1,5 @@
+
+
 #define __ooc_kern(rest)  __ooc_kern_ ## rest
 #define __ooc_void        void __ooc_kern (
 #define __ooc_1__(rest)   __ooc_ ## rest
@@ -15,10 +17,10 @@ __ooc__((( void mm() )))
 
 
 #define __ooc_kern(rest)  __ooc_kern_ ## rest
-#define __ooc_void        void __ooc_kern (
-#define __ooc_1__(rest)   __ooc_ ## rest )
-#define __ooc__           __ooc_1__ (
-__ooc__ void mm() )
+#define __ooc_void        void __ooc_kern(
+#define __ooc_1__(rest)   __ooc_ ## rest)
+#define __ooc__           __ooc_1__(
+__ooc__ void mm(void) )
 {
   return;
 }
@@ -29,23 +31,34 @@ __ooc__ void mm() )
 
 
 #define __ooc_kern(rest)  __ooc_kern_ ## rest
-#define __ooc_void        void __ooc_kern (
-#define __ooc__(rest)     __ooc_ ## rest )
-__ooc__( void mm() )
+#define __ooc_void        void __ooc_kern(
+#define __ooc_1__(rest)   __ooc_ ## rest)
+#define __ooc__           __ooc_1__
+__ooc__ ( void mm(void) )
 {
   return;
 }
 #undef __ooc_kern
 #undef __ooc_void
+#undef __ooc_1__
 #undef __ooc__
 
 
-/*#define ooc_for(loop) \
-  __ooc_kern_ ## call;*/
-
+#define __ooc__
+#define ooc_for(loop) \
+  for (loop) {
+#define ooc_rof \
+  }
 
 /* Application code */
 
-ooc_for (i=0; i<10; ++i) {
+__ooc__ void mm(void)
+{
+  return;
+}
+
+ooc_for (i=0; i<m; ++i)
+{
   mm(n, p, a, b, c);
 }
+ooc_rof
