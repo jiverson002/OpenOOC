d6069cbf29468949ce7f9f6df6cf6a571af94649
jeremy <jliverson5@gmail.com>
2016-04-13 13:02:54 -0500

    overhauled run.sh to be more configurable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,34 +1,73 @@
 #! /bin/bash
 
-LOG=run.log
-
-HLINE="\n================================================================================\n"
-hline="\n--------------------------------------------------------------------------------\n"
-
 function run() {
   echo "$@" >> $LOG
-  /usr/bin/time -ao $LOG $@ >> $LOG
+  $TIMEEXE $@ >> $LOG 2>&1
 }
 
 function build() {
-  run build/bin/mm -n1024 -m450560 -p3072                        -t4 $1
+  N=$((T*2**GLG))
+  M=$((28835840*GB/T/2**LLG))
+  P=$((T*3*2**GLG))
+  run build/bin/mm -n$N -m$M -p$P -t$T -f$1
   echo -e "$hline" >> $LOG
-  run build/bin/mm -n1024 -m450560 -p3072 -y64   -x450560 -z192  -t4 $1
-  echo -e "$hline" >> $LOG
-  run build/bin/mm -n1024 -m450560 -p3072 -y128  -x225280 -z384  -t4 $1
-  echo -e "$hline" >> $LOG
-  run build/bin/mm -n1024 -m450560 -p3072 -y256  -x112640 -z768  -t4 $1
-  echo -e "$hline" >> $LOG
-  run build/bin/mm -n1024 -m450560 -p3072 -y512  -x56320  -z1536 -t4 $1
-  echo -e "$hline" >> $LOG
-  run build/bin/mm -n1024 -m450560 -p3072 -y1024 -x28160  -z3072 -t4 $1
+  for ((i=$LLG; i<=$GLG; ++i)) ; do
+    Y=$((N/2**(GLG-i)))
+    X=$((M/2**(i-LLG)))
+    Z=$((P/2**(GLG-i)))
+    if (($1*$T<=$Y)) ; then
+      run build/bin/mm -n$N -m$M -p$P -y$Y -x$X -z$Z -t$T -f$1
+      echo -e "$hline" >> $LOG
+    fi
+  done
   echo -e "$HLINE" >> $LOG
 }
 
-build -f0
-build -f1
-build -f16
-build -f32
-build -f64
-build -f128
-build -f256
+# Default params
+TIMEEXE=/usr/bin/time
+T=4
+GB=1
+POWS="1 2 4 8 16 32 64 128 256 512 1024 2048"
+TEMPLATE="run"
+HLINE="\n================================================================================\n"
+hline="\n--------------------------------------------------------------------------------\n"
+
+# Handle input
+if [ $# -eq 1 ] ; then
+  OPTS=$1
+else
+  OPTS=0
+fi
+
+# Increment the identifier in the filename until the filename is unique
+LOG="$TEMPLATE-0"
+while [ -e $LOG ] ; do
+  ID=${LOG##*-}
+  ID=$((ID+1))
+  LOG="$TEMPLATE-$ID"
+done
+
+# Find lowest power of 2
+i=$OPTS
+LLG=0
+for i in $POWS ; do
+  if (($i==($OPTS&$i))) ; then
+    break
+  fi
+  LLG=$((LLG+1))
+done
+
+# Find greatest power of 2
+i=$OPTS
+GLG=-1
+while (($i>0)) ; do
+  GLG=$((GLG+1))
+  i=$((i/2))
+done
+
+# Execute the tests
+for i in 0 $POWS ; do
+  if (($i==($OPTS&$i))) ; then
+    build $i
+  fi
+done
