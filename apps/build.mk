e8834f04c743a523bcdfe8d473b7e64012efc8a1
jeremy <jliverson5@gmail.com>
2016-04-18 12:06:22 -0500

    moved swap.c from apps to bench directory, since it is a benchmark program

--- apps/build.mk
+++ apps/build.mk
@@ -1,4 +1,4 @@
-apps_PROGRAMS := matmult mlock samplesort swap vecsort
+apps_PROGRAMS := matmult mlock samplesort vecsort
 apps_LDADD    := libooc.a
 apps_CFLAGS   := -fopenmp
 apps_LDFLAGS  := -fopenmp
@@ -6,8 +6,6 @@ apps_LDFLAGS  := -fopenmp
 matmult_SOURCES := matmult.c
 
 mlock_SOURCES := mlock.c
-
-swap_SOURCES := swap.c
 
 samplesort_SOURCES := samplesort.c
 
--- bench/build.mk
+++ bench/build.mk
@@ -1,7 +1,14 @@
-bench_PROGRAMS := micro micro-latency
-bench_LDLIBS = -lrt
+bench_PROGRAMS := micro micro-latency swap
+bench_CFLAGS   := -fopenmp
 
 micro_SOURCES  := impl/io.c impl/libc.c impl/sbma.c micro.c
 micro_HEADERS  := impl/impl.h
+micro_LDLIBS   := -lrt
 
 micro-latency_SOURCES := micro-latency.c
+micro-latency_LDLIBS  := -lrt
+
+swap_SOURCES := swap.c
+swap_LDADD   := libooc.a
+swap_LDFLAGS := -fopenmp
+#swap_LDFLAGS := -fopenmp -ggdb
