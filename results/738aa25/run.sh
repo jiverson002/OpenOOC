66f7061d24fde6bd29c3da1f6d13b6c53cdf00e3
jeremy <jliverson5@gmail.com>
2017-06-21 11:06:07 -0500

    added results along with the scripts that generated them

---
+++ results/738aa25/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CMD="/usr/bin/time build/bin/matmult"
+LOG="run.out"
+
+function run() {
+  OBLOCKS=`cat /proc/diskstats | grep sdb1 | awk '{print $6}'`
+  eval "echo $CMD $@ >> $LOG"
+  eval "$CMD $@ >> $LOG 2>&1"
+  NBLOCKS=`cat /proc/diskstats | grep sdb1 | awk '{print $6}'`
+  printf "  I/O (GB)     = %9.5f\n" `echo "scale=10; ($NBLOCKS - $OBLOCKS) * 512 / 1024^3" | bc` >> $LOG
+}
+
+for l in $1 ; do
+  for f in 0 1 2 4 8 16 32 64 128 256 440 ; do
+    #sudo fstrim /scratch-ssd
+    #run -l$l -n1760 -m262144 -p1760 -y1    -x1     -z1    -t4 -f$f
+    sudo fstrim /scratch-ssd
+    run -l$l -n1760 -m262144 -p1760 -y1760 -x32768 -z1760 -t4 -f$f
+  done
+done
