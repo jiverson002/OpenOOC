17bc115ee0f5721033dbbe763635ded4622d3e4e
jeremy <jliverson5@gmail.com>
2017-06-26 13:27:13 -0500

    rewrote floyd to use 2D blocking to try and reuse data in memory as much as possible

--- apps/floyd.c
+++ apps/floyd.c
@@ -24,6 +24,9 @@ THE SOFTWARE.
 /* assert */
 #include <assert.h>
 
+/* DBL_EPSILON */
+#include <float.h>
+
 /* HOST_NAME_MAX */
 #include <limits.h>
 
@@ -69,8 +72,8 @@ THE SOFTWARE.
 struct args
 {
   size_t n;
-  size_t x;
   size_t k;
+  size_t js, je;
   double * restrict a;
 };
 
@@ -91,42 +94,23 @@ S_matfill(size_t const n, double * const a)
 static void
 S_floyd_kern(size_t const i, void * const state)
 {
-  size_t n, x, k, js, je;
+  size_t n, j, k, js, je;
   double av, bv;
-  double * restrict a, * ap, * as, * cp, * cs, * ce;
+  double * restrict a;
   struct args const * args;
 
   args = (struct args const*)state;
   n = args->n;
-  x = args->x;
   k = args->k;
+  js = args->js;
+  je = args->je;
   a = args->a;
 
-  bv = a(i, k);   // dist[i][k]
-
-  if (1 == x) { /* standard */
-    as = &a(k, 0);  // dist[k][*]
-    cs = &a(i, 0);  // dist[i][*]
-    ce = cs + n;
-    for (ap=as,cp=cs; cp<ce; ++ap,++cp) {
-      av = *ap;
-      if (*cp > bv + av) {
-        *cp = bv + av;
-      }
-    }
-  }
-  else {        /* tiled */
-    for (js=0; js<n; js+=x) {
-      je = js+x < n ? js+x : n;
-      as = &a(k, js); // dist[k][*]
-      cs = &a(i, js); // dist[i][*]
-      ce = &a(i, je);
-      for (ap=as,cp=cs; cp<ce; ++ap,++cp) {
-        av = *ap;
-        if (*cp > bv + av) {
-          *cp = bv + av;
-        }
-      }
+  bv = a(i, k);
+  for (j=js; j<je; ++j) {
+    av = a(k, j);
+    if (a(i, j) > bv + av) {
+      a(i, j) = bv + av;
     }
   }
 }
@@ -141,19 +125,186 @@ __ooc_defn ( static void S_floyd_ooc )(size_t const i, void * const state)
 }
 
 
+static void
+S_floyd_block_os(struct args * const restrict args, size_t const is, size_t const ie, size_t const ks, size_t const ke)
+{
+  size_t i, k;
+  for (k=ks; k<ke; ++k) { 
+    args->k = k;
+
+    #pragma omp for nowait
+    for (i=is; i<ie; ++i) {
+      S_floyd_kern(i, args);
+    }
+    #pragma omp barrier
+  }
+}
+
+
+static void
+S_floyd_block_ooc(struct args * const restrict args, size_t const is, size_t const ie, size_t const ks, size_t const ke)
+{
+  int ret;
+  size_t i, k;
+  size_t const n = args->n;
+  double * const restrict a = args->a;
+  for (k=ks; k<ke; ++k) {
+    args->k = k;
+
+    #pragma omp for nowait
+    for (i=is; i<ie; ++i) {
+      S_floyd_ooc(i, args);
+    }
+    ooc_wait(); /* Need this to wait for any outstanding fibers. */
+    #pragma omp barrier
+
+    #pragma omp single
+    {
+      /* `flush pages`, i.e., undo memory protections, so that
+       * fibers are able to be invoked the next iteration that this
+       * memory is touched. */
+      ret = mprotect(a+k*n, n*sizeof(*(a)), PROT_NONE);
+      assert(!ret);
+    }
+  }
+}
+
+
+static void
+S_floyd_untiled(size_t const n, size_t const x, double * const restrict a, void (*floyd_block)(struct args*, size_t, size_t, size_t, size_t))
+{
+  int ret;
+  size_t k;
+  struct args args;
+
+  /* Setup args struct. */
+  args.n = n;
+  args.a = a;
+  args.js = 0;
+  args.je = n;
+
+  for (k=0; k<n; ++k) {
+    // compute entire matrix as a single block
+    floyd_block(&args, 0, n, k, k+1);
+
+    // FIXME: hack
+    if (floyd_block == S_floyd_block_ooc) {
+      #pragma omp single
+      {
+        /* `flush pages`, i.e., undo memory protections, so that fibers
+         * are able to be invoked the next iteration that this memory is
+         * touched. */
+        ret = mprotect(a, n*n*sizeof(*a), PROT_NONE);
+        assert(!ret);
+      }
+    }
+  }
+
+  /* suppress unused parameter warning */
+  (void)x;
+}
+
+
+static void
+S_floyd_tiled(size_t const n, size_t const x, double * const restrict a, void (*floyd_block)(struct args*, size_t, size_t, size_t, size_t))
+{
+  int ret;
+  size_t is, ie, js, je, ks, ke;
+  struct args args;
+
+  /* Setup args struct. */
+  args.n = n;
+  args.a = a;
+
+  for (ks=0; ks<n; ks+=x) {
+    ke = ks+x < n ? ks+x : n;
+
+    // compute block (ks,ks)
+    is = ks;
+    ie = ke;
+    args.js = ks;
+    args.je = ke;
+    floyd_block(&args, ks, ke, is, ie);
+
+    // compute blocks (ks, *)
+    is = ks;
+    ie = ke;
+    for (js=0; js<n; js+=x) {
+      je = js+x < n ? js+x : n;
+      args.js = js;
+      args.je = je;
+
+      // skip block (ks,ks), it has already been computed
+      if (js == ks) {
+        continue;
+      }
+
+      floyd_block(&args, ks, ke, is, ie);
+    }
+
+    // compute blocks (*, ks)
+    args.js = ks;
+    args.je = ke;
+    for (is=0; is<n; is+=x) {
+      ie = is+x < n ? is+x : n;
+
+      // skip block (ks,ks), it has already been computed
+      if (is == ks) {
+        continue;
+      }
+
+      floyd_block(&args, ks, ke, is, ie);
+    }
+
+    // compute blocks (*, *)
+    for (is=0; is<n; is+=x) {
+      ie = is+x < n ? is+x : n;
+
+      // skip block (ks,*), it has already been computed
+      if (is == ks) {
+        continue;
+      }
+
+      for (js=0; js<n; js+=x) {
+        je = js+x < n ? js+x : n;
+        args.js = js;
+        args.je = je;
+
+        // skip block (*,ks), it has already been computed
+        if (js == ks) {
+          continue;
+        }
+
+        floyd_block(&args, ks, ke, is, ie);
+      }
+    }
+
+    // FIXME: hack
+    if (floyd_block == S_floyd_block_ooc) {
+      #pragma omp single
+      {
+        /* `flush pages`, i.e., undo memory protections, so that fibers are able
+         * to be invoked the next iteration that this memory is touched. */
+        ret = mprotect(a, n*n*sizeof(*a), PROT_NONE);
+        assert(!ret);
+      }
+    }
+  }
+}
+
+
 int
 main(int argc, char * argv[])
 {
   int ret, opt, num_fibers, num_threads, validate;
   double ts, te, t1, t2, t3;
   size_t lock;
-  size_t n, y, x;
+  size_t n, x;
   size_t i, j, k;
-  size_t is, ie;
   time_t now;
-  struct args args;
   void * l=NULL;
   double * a, * v;
+  void (*floyd)(size_t, size_t, double *, void (*)(struct args*, size_t , size_t, size_t, size_t));
   char hostname[HOST_NAME_MAX];
 
   now = time(NULL);
@@ -162,10 +313,9 @@ main(int argc, char * argv[])
   validate = 0;
   n = 32768;
   x = 1;
-  y = 1;
   num_fibers = 0;
   num_threads = 1;
-  while (-1 != (opt=getopt(argc, argv, "vl:n:x:y:f:t:"))) {
+  while (-1 != (opt=getopt(argc, argv, "vl:n:x:f:t:"))) {
     switch (opt) {
       case 'l':
       lock = (size_t)atol(optarg);
@@ -191,13 +341,9 @@ main(int argc, char * argv[])
       x = (size_t)atol(optarg);
       break;
 
-      case 'y':
-      y = (size_t)atol(optarg);
-      break;
-
       default: /* '?' */
         fprintf(stderr, "Usage: %s [-dv] [-n n dim] "\
-          "[-x x dim] [-y y dim] [-f num_fibers] [-t num_threads]\n",\
+          "[-x x dim] [-f num_fibers] [-t num_threads]\n",\
           argv[0]);
         return EXIT_FAILURE;
     }
@@ -208,15 +354,7 @@ main(int argc, char * argv[])
   assert(num_fibers >= 0);
 
   /* Fix-up input. */
-  y = (y < n) ? y : n;
   x = (x < n) ? x : n;
-
-  /* Lock desired amount of memory. */
-  if (lock) {
-    l = mmap(NULL, lock, PROT_READ|PROT_WRITE,\
-      MAP_PRIVATE|MAP_ANONYMOUS|MAP_LOCKED, -1, 0);
-    assert(MAP_FAILED != l);
-  }
 
   /* Allocate memory. */
   a = mmap(NULL, n*n*sizeof(*a), PROT_READ|PROT_WRITE,\
@@ -253,7 +391,6 @@ main(int argc, char * argv[])
   printf("===========================\n");
   printf("  lock memory  = %10zu\n", lock);
   printf("  n            = %10zu\n", n);
-  printf("  y            = %10zu\n", y);
   printf("  x            = %10zu\n", x);
   printf("  # fibers     = %10d\n", num_fibers);
   printf("  # threads    = %10d\n", num_threads);
@@ -272,7 +409,7 @@ main(int argc, char * argv[])
 
   /* Copy original matrix for validation */
   if (validate) {
-    memcpy(v, a, n*n*sizeof(*a));
+    memcpy(v, a, n*n*sizeof(*v));
   }
 
   if (num_fibers) {
@@ -280,110 +417,38 @@ main(int argc, char * argv[])
     assert(!ret);
   }
 
-  /* Setup args struct. */
-  args.n = n;
-  args.x = x;
-  args.a = a;
+  /* Lock desired amount of memory. */
+  if (lock) {
+    l = mmap(NULL, lock, PROT_READ|PROT_WRITE,\
+      MAP_PRIVATE|MAP_ANONYMOUS|MAP_LOCKED, -1, 0);
+    assert(MAP_FAILED != l);
+  }
 
   printf("  Computing Floyd all-pairs shortest path...\n");
   ts = omp_get_wtime();
-  if (1 == y) { /* standard */
-    if (num_fibers) {
-      #pragma omp parallel num_threads(num_threads) private(is,ie,k,ret)
-      {
-        /* Prepare OOC environment. */
-        ooc_set_num_fibers((unsigned int)num_fibers);
+  if (1 == x) {     /* standard */
+    floyd = S_floyd_untiled;
+  }
+  else {            /* tiled */
+    floyd = S_floyd_tiled;
+  }
+  if (num_fibers) { /* ooc */
+    #pragma omp parallel num_threads(num_threads) private(ret)
+    {
+      /* Prepare OOC environment. */
+      ooc_set_num_fibers((unsigned int)num_fibers);
 
-        for (k=0; k<n; ++k) {
-          #pragma omp single
-          {
-            args.k = k;
-          }
+      floyd(n, x, a, S_floyd_block_ooc);
 
-          #pragma omp for nowait
-          for (i=0; i<n; ++i) {
-            S_floyd_ooc(i, &args);
-          }
-          ooc_wait(); /* Need this to wait for any outstanding fibers. */
-          #pragma omp barrier
-
-          #pragma omp single
-          {
-            /* `flush pages`, i.e., undo memory protections, so that fibers
-             * are able to be invoked the next iteration that this memory is
-             * touched. */
-            ret = mprotect(a, n*n*sizeof(*a), PROT_NONE);
-            assert(!ret);
-          }
-        }
-
-        /* Finalize OOC environment. */
-        ret = ooc_finalize(); /* Need this to and remove the signal handler. */
-        assert(!ret);
-      }
-    }
-    else {
-      for (k=0; k<n; ++k) {
-        args.k = k;
-        #pragma omp parallel for num_threads(num_threads)
-        for (i=0; i<n; ++i) {
-          S_floyd_kern(i, &args);
-        }
-      }
+      /* Finalize OOC environment. */
+      ret = ooc_finalize(); /* Need this to and remove the signal handler. */
+      assert(!ret);
     }
   }
-  else {        /* tiled */
-    if (num_fibers) {
-      #pragma omp parallel num_threads(num_threads) private(is,ie,k,ret)
-      {
-        /* Prepare OOC environment. */
-        ooc_set_num_fibers((unsigned int)num_fibers);
-
-        for (k=0; k<n; ++k) {
-          #pragma omp single
-          {
-            args.k = k;
-          }
-
-          for (is=0; is<n; is+=y) {
-            ie = is+y < n ? is+y : n;
-
-            #pragma omp for nowait
-            for (i=is; i<ie; ++i) {
-              S_floyd_ooc(i, &args);
-            }
-            ooc_wait(); /* Need this to wait for any outstanding fibers. */
-            #pragma omp barrier
-          }
-
-          #pragma omp single
-          {
-            /* `flush pages`, i.e., undo memory protections, so that fibers
-             * are able to be invoked the next iteration that this memory is
-             * touched. */
-            ret = mprotect(a, n*n*sizeof(*a), PROT_NONE);
-            assert(!ret);
-          }
-        }
-
-        /* Finalize OOC environment. */
-        ret = ooc_finalize(); /* Need this to and remove the signal handler. */
-        assert(!ret);
-      }
-    }
-    else {
-      for (k=0; k<n; ++k) {
-        args.k = k;
-
-        for (is=0; is<n; is+=y) {
-          ie = is+y < n ? is+y : n;
-
-          #pragma omp parallel for num_threads(num_threads)
-          for (i=is; i<ie; ++i) {
-            S_floyd_kern(i, &args);
-          }
-        }
-      }
+  else {            /* os */
+    #pragma omp parallel num_threads(num_threads)
+    {
+      floyd(n, x, a, S_floyd_block_os);
     }
   }
   te = omp_get_wtime();
@@ -415,7 +480,10 @@ main(int argc, char * argv[])
     }
     for (i=0; i<n; ++i) {
       for (j=0; j<n; ++j) {
-        assert(v(i,j) == a(i,j));
+        if (v(i,j) - a(i,j) > DBL_EPSILON) {
+          printf("%f %f\n", v(i,j), a(i,j));
+        }
+        assert(v(i,j) - a(i,j) <= DBL_EPSILON);
       }
     }
     te = omp_get_wtime();
